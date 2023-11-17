8c26702d6ff24bf1bd74fa9169846573313d3d46
darina smirnova <darismi01@yandex.ru>
2023-11-17 23:43:40 +0300

    Revert "hello word"
    This reverts commit e629dca24a1b93541229bbedfacb969d875a79c2.

--- git_project/src/main.py
+++ git_project/src/main.py
@@ -1,1 +1,11 @@
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+#!/usr/bin/python3
+print("Hello Linux")
+
+=======
+>>>>>>> parent of 21e3faf (Hello Word)
+>>>>>>> parent of e629dca (hello word)
